026562b565cb31a25ed726aad8d0b1df99728412
Kirill <apatski94@gmail.com>
2023-03-29 01:43:55 +0300

    add routing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Header.module.scss'
+import {Link} from "react-router-dom";
+import {PATH} from "../Pages/Pages";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1>Currency Converter</h1>
       <ul className={styles.linkBox}>
-        <li className={styles.link}><a href="#">Converter</a></li>
-        <li className={styles.link}><a href="#">Exchange rate</a></li>
+        <li className={styles.link}><Link to={PATH.converter}>Converter</Link></li>
+        <li className={styles.link}><Link to={PATH.exchangeRate}>Exchange rate</Link></li>
       </ul>
     </header>
   );
