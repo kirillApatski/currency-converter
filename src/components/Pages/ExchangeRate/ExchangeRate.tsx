3f2e4c9f9912aafba1712fd8a6f3062495144f0b
Kirill <apatski94@gmail.com>
2023-03-30 00:15:09 +0300

    added HTML and styles in component ExchangeRate

--- src/components/Pages/ExchangeRate/ExchangeRate.tsx
+++ src/components/Pages/ExchangeRate/ExchangeRate.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import styles from './ExchangeRate.module.scss'
+
 
 const ExchangeRate = () => {
+
+
   return (
-    <div>
-      Exchange rate
+    <div className={styles.wrapper}>
+      <h2>Exchange Rate</h2>
+      <ul className={styles.exchangeRateBox}>
+        <li className={styles.rate}>1 USD - 2.84</li>
+        <li className={styles.rate}>1 PLN - 6.45</li>
+        <li className={styles.rate}>1 EUR - 3.41</li>
+        <li className={styles.rate}>100 RUR - 35.71</li>
+      </ul>
     </div>
   );
 };
