804a81cb8856b7c9c6a13b6427dbf03008130b66
Kirill <apatski94@gmail.com>
2023-03-30 00:13:50 +0300

    refactoring changed Link to NavLink and add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from './Header.module.scss'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {PATH} from "../Pages/Pages";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Currency Converter</h1>
+      <h1>Currency Converter <span>and Exchange rate</span></h1>
       <ul className={styles.linkBox}>
-        <li className={styles.link}><Link to={PATH.converter}>Converter</Link></li>
-        <li className={styles.link}><Link to={PATH.exchangeRate}>Exchange rate</Link></li>
+        <li className={styles.link}><NavLink className={({ isActive }) => isActive ? styles.active : ""} to={PATH.converter}>Converter</NavLink></li>
+        <li className={styles.link}><NavLink className={({ isActive }) => isActive ? styles.active : ""} to={PATH.exchangeRate}>Exchange rate</NavLink></li>
       </ul>
     </header>
   );
