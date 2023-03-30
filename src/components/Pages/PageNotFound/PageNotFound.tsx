17b9a7a7c4d4fcadf40bc726ac213fa8e3a0b4a6
Kirill <apatski94@gmail.com>
2023-03-30 19:03:32 +0300

    added test currencyReducer and add refactoring PageNotFound

--- src/components/Pages/PageNotFound/PageNotFound.tsx
+++ src/components/Pages/PageNotFound/PageNotFound.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import styles from './PageNotFound.module.scss'
 
 const PageNotFound = () => {
   return (
-    <div>
-      404
-    </div>
+    <h2 className={styles.pageNotFound}>404 PAGE NOT FOUND</h2>
   );
 };
 
---
+++ src/tests/currencyReducer.test.ts
@@ -0,0 +1,62 @@
+import {
+  changeCurrentCurrency,
+  currencyReducer,
+  CurrencyState,
+  saveCurrencyData,
+  saveResultSum
+} from "../redusers/currencyReducer";
+
+let startState: CurrencyState;
+
+beforeEach(() => {
+  startState = {
+    currencies: [
+      {
+        Cur_ID: 1,
+        Date: "2023-03-30T00:00:00",
+        Cur_Abbreviation: "EUR",
+        Cur_Name: "Евро",
+        Cur_OfficialRate: 3.1013,
+        Cur_Scale: 1
+      }
+    ],
+    currentCurrency: 'USD',
+    resultSum: 0
+  };
+});
+
+test('save currency data', () => {
+  const action = saveCurrencyData([{
+    Cur_ID: 2,
+    Date: "2023-02-20T00:00:00",
+    Cur_Abbreviation: "RUB",
+    Cur_Name: "Российских рублей",
+    Cur_OfficialRate:
+      3.7237,
+    Cur_Scale: 100
+  }])
+
+  const endState = currencyReducer(startState, action)
+
+  expect(endState.currencies.length).toBe(1);
+  expect(endState.currencies[0].Cur_Abbreviation).toBe("RUB");
+  expect(endState.currencies).toBeDefined();
+});
+
+test('change current currency', () => {
+  const action = changeCurrentCurrency('USD')
+
+  const endState = currencyReducer(startState, action)
+
+  expect(endState.currencies).toBeDefined();
+  expect(endState.currentCurrency).toBe("USD");
+});
+
+test('save result sum', () => {
+  const action = saveResultSum(555)
+
+  const endState = currencyReducer(startState, action)
+
+  expect(endState.resultSum).toBeDefined();
+  expect(endState.resultSum).toBe(555);
+});
